a5a05e70920c4f9d1b84ecdc52b0c5874e16eb9e
lloydzhou <lloydzhou@qq.com>
2023-02-20 13:58:06 +0800

    add update event #2

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -14,13 +14,31 @@ export const useGraphState = (initState: GraphState = {}) => {
   const diffEdges = useMemo(() => diffCells(graph.current, edges, 'edge'), [nodes, edges]);
 
   const setGraph = useCallback((_g: Graph) => { if (_g) { graph.current = _g; } }, []);
-  // 更新state数据之前先检查id是否存在，自动创建id，确保diffCells的时候能使用id进行判断
-  const setNodes = (_nodes: Node.Metadata[]) => _setNodes(_nodes.map(checkId));
-  const setEdges = (_edges: Edge.Metadata[]) => _setEdges(_edges.map(checkId));
 
+  const eventName = 'update:state'
+  // 注册一个自定义事件，patch完成后触发
+  const returnPromise = (execute: () => void) => {
+    return new Promise((resolve) => {
+      if (graph.current) {
+        graph.current.once(eventName, resolve)
+      }
+      execute()
+    })
+  }
+  // 更新state数据之前先检查id是否存在，自动创建id，确保diffCells的时候能使用id进行判断
+  const setNodes = (_nodes: Node.Metadata[]) => returnPromise(() => _setNodes(_nodes.map(checkId)));
+  const setEdges = (_edges: Edge.Metadata[]) => returnPromise(() => _setEdges(_edges.map(checkId)));
+
+  const triggerEvent = () => graph.current && graph.current.trigger(eventName)
   // 使用patch函数更新数据到x6画布
-  useEffect(() => patch(graph.current, diffNodes), [diffNodes]);
-  useEffect(() => patch(graph.current, diffEdges), [diffEdges]);
+  useEffect(() => {
+    patch(graph.current, diffNodes)
+    triggerEvent()
+  }, [diffNodes]);
+  useEffect(() => {
+    patch(graph.current, diffEdges)
+    triggerEvent()
+  }, [diffEdges]);
 
   return {
     nodes, edges, graph, setNodes, setEdges, setGraph,
